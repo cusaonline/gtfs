d2f977e920cebbb5d220eb2cba1d638a8db34c69
CactusAcolyte <67842545+CactusAcolyte@users.noreply.github.com>
2026-08-20 10:10:59 -0400

    Very early realtime-only prototype

--- src/__main__.py
+++ src/__main__.py
@@ -11,11 +11,13 @@ import json
 import zipfile
 import sqlite3
 import pandas
+import datetime
+from operator import itemgetter
 
 def gtfs_schedule_request():
     return requests.get("https://oct-gtfs-emasagcnfmcgeham.z01.azurefd.net/public-access/GTFSExport.zip")
 
-# TODO: merge the safe requests dev branch
+# TODO: merge the safe requests fork by cutie
 # format can be 'json' or 'protobuf'
 def gtfs_realtime_request(format = 'json'):
     param = {'format': format}
@@ -47,12 +49,50 @@ def gtfs_initialize_database():
             data = pandas.read_csv('gtfs_static/' + file, low_memory=False)
             data.to_sql(file.split('.')[0], conn, index=False, if_exists='replace')
 
+# TODO: actually integrate schedule data
 def gtfs_signboard_update(stop_list):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(gtfs_realtime_request('protobuf').content)
+
     with sqlite3.connect('gtfs.db') as conn:
         curr = conn.cursor()
         for stop, name in stop_list.items():
-            for row in curr.execute("SELECT * FROM stops WHERE stop_id = :stop", {"stop": stop}):
-                print("%s (%s): %s"%(name, row[1] + ("%s"%row[14] if row[14] else "") , "info"))
+            for row in curr.execute('SELECT * FROM stops WHERE stop_id = :stop', {'stop': stop}):
+                print("%s (%s): "%(name, row[1] + ("%s"%row[14] if row[14] else "")), end="")
+                info = gtfs_get_info(feed, conn, stop)
+                for x in range(len(info)):
+
+                    print("%s %s @ %s"%(info[x][0], info[x][1], datetime.datetime.fromtimestamp(info[x][2]).strftime("%H:%M")), end="")
+
+                    if x == len(info)-1:
+                        print(".")
+                    else:
+                        print(", ", end="")
+
+def gtfs_get_info(feed, conn, stop):
+    info = list()
+    curr = conn.cursor()
+    for entity in feed.entity:
+        if entity.HasField('trip_update'):
+            for update in entity.trip_update.stop_time_update:
+                if update.HasField('stop_id') and update.stop_id == stop:
+                    trip_id = entity.trip_update.trip.trip_id
+                    route_id = entity.trip_update.trip.route_id
+                    # TODO: move this query junk to other function. keep all the squirrels in one place
+                    headsign = ""
+                    for row in curr.execute('SELECT trip_headsign FROM trips WHERE trip_id = :trip_id',
+                                            {'trip_id': trip_id}):
+                        headsign = row[0]
+                    arrv_time = int()
+                    if update.HasField('arrival') and update.arrival.HasField('time'):
+                        arrv_time = update.arrival.time
+                    dprt_time = int()
+                    if update.HasField('departure') and update.departure.HasField('time'):
+                        dprt_time = update.departure.time
+                    stop_time = max(arrv_time, dprt_time)
+                    info.append((route_id, headsign, stop_time))
+    info.sort(key=itemgetter(2))
+    return info
 
 
 if __name__ == '__main__':
@@ -67,11 +107,8 @@ if __name__ == '__main__':
                "10738": "Athletics",
                "10147": "Nesbitt",
                "10146": "TT Centre",
-               "10145": "Stadium",
-               "CG990": "O-Train North",
-               "CG995": "O-Train South"}
+               "10145": "Stadium"}
 
     gtfs_signboard_update(stop_list)
 
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(gtfs_realtime_request('protobuf').content)
+
