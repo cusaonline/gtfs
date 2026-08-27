dcf65c6433229efdb36a80064702db01e6acf8ed
CactusAcolyte <67842545+CactusAcolyte@users.noreply.github.com>
2026-08-27 11:03:50 -0400

    Finish refactor!

--- src/__main__.py
+++ src/__main__.py
@@ -180,30 +180,35 @@ def signboard_from_realtime(stop_list: list[str]) -> Signboard:
     feed.ParseFromString(gtfs_realtime_request('protobuf').content)
 
     rt_signboard = Signboard(dt.datetime.now(tz=get_agency_timezone()), [])
+    for stop_id in stop_list:
+        rt_signboard.stops.append(stop_from_id(stop_id))
 
     for entity in feed.entity:
         if entity.HasField('trip_update'):
             for update in entity.trip_update.stop_time_update:
-                if update.HasField('stop_id') and update.stop_id in stop_list:
+                if update.HasField('stop_id') and update.stop_id in rt_signboard:
                     rt_stop_id = update.stop_id
-                    print(entity.trip_update.trip.HasField('trip_id'))
-                    print(entity.trip_update.trip.trip_id)
                     rt_trip_id = entity.trip_update.trip.trip_id
                     rt_scheduled = TripType(entity.trip_update.trip.schedule_relationship)
 
-                    print(rt_stop_id, rt_trip_id)
+                    # TODO: temporary kludge to stop crashes. throw out "new" buses
+                    if rt_scheduled == TripType.NEW:
+                        break
+
                     scheduled_trip = scheduled_trip_from_id(rt_trip_id, rt_stop_id)
 
                     rt_headsign = scheduled_trip.headsign
                     rt_bus_time = scheduled_trip.bus_time
-                    rt_bus_num = None
-                    if entity.trip_update.HasField('vehicle') and entity.trip_update.vehicle.HasField('id'):
-                        bus_num = entity.trip_update.vehicle.id
 
                     if update.HasField('arrival') and update.arrival.HasField('time'):
-                        bus_time = dt.datetime.fromtimestamp(update.arrival.time, get_agency_timezone())
+                        rt_bus_time = dt.datetime.fromtimestamp(update.arrival.time, get_agency_timezone())
                     if update.HasField('departure') and update.departure.HasField('time'):
-                        bus_time = dt.datetime.fromtimestamp(update.arrival.time, get_agency_timezone())
+                        rt_bus_time = dt.datetime.fromtimestamp(update.arrival.time, get_agency_timezone())
+
+                    rt_bus_num = None
+
+                    if entity.trip_update.HasField('vehicle') and entity.trip_update.vehicle.HasField('id'):
+                        rt_bus_num = entity.trip_update.vehicle.id
 
                     trip_to_add = Trip(rt_trip_id, rt_bus_time, True, rt_scheduled, rt_headsign, rt_bus_num)
                     route_to_add = route_from_trip(trip_to_add)
@@ -220,22 +225,18 @@ def signboard_from_realtime(stop_list: list[str]) -> Signboard:
 
     return rt_signboard
 
-
-# TODO: implement
+# TODO: write this eventually
 def signboard_from_schedule() -> Signboard:
     pass
     # return Signboard(stops=[],
     #                  sign_time=dt.datetime.now(tz=get_agency_timezone()))
 
 
-# TODO: merge or modernize functions south of this line
-def gtfs_schedule_request():
+def gtfs_schedule_request() -> requests.Response:
     return requests.get(config['sources']['schedule'])
-
-
 # TODO: merge the safe requests fork by cutie
 # format can be 'json' or 'protobuf'
-def gtfs_realtime_request(format='json'):
+def gtfs_realtime_request(format='json') -> requests.Response:
     param = {'format': format}
     api_key = os.getenv('OC_API_KEY')
     header = {'Ocp-Apim-Subscription-Key': api_key}
@@ -243,18 +244,18 @@ def gtfs_realtime_request(format='json'):
     return requests.get(config['sources']['realtime'], params=param, headers=header)
 
 
-def gtfs_schedule_update():
+def gtfs_schedule_update() -> None:
     with open(config['path']['data'] + 'gtfs_static.zip', 'bw+') as s:
         s.write(gtfs_schedule_request().content)
 
 
-def gtfs_realtime_update():
+def gtfs_realtime_update() -> None:
     with open(config['path']['data'] + 'gtfs_update.json', 'w+') as r:
         js = json.loads(gtfs_realtime_request('json').content)
         r.write(json.dumps(js, indent=2))
 
 
-def gtfs_initialize_database():
+def gtfs_initialize_database() -> None:
     gtfs_schedule_update()
 
     with zipfile.ZipFile(config['path']['data'] + 'gtfs_static.zip', 'r') as zipf:
@@ -267,76 +268,18 @@ def gtfs_initialize_database():
             data = pandas.read_csv(config['path']['data'] + 'gtfs_static/' + file, low_memory=False)
             data.to_sql(file.split('.')[0], conn, index=False, if_exists='replace')
 
-
-# TODO: actually integrate schedule data
-def gtfs_signboard_update(stop_list):
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(gtfs_realtime_request('protobuf').content)
-
-    # TODO: un-kludge solution to output to webpage
-    output = ""
-
-    with sqlite3.connect(config['path']['data'] + 'gtfs.db') as conn:
-        curr = conn.cursor()
-        for stop, name in stop_list.items():
-            for row in curr.execute('SELECT * FROM stops WHERE stop_id = :stop', {'stop': stop}):
-                output += "%s (%s): " % (name, row[1] + ("%s" % row[14] if row[14] else ""))
-                info = gtfs_get_info(feed, conn, stop)
-                for x in range(len(info)):
-
-                    output += "%s %s @ %s" % (info[x][0], info[x][1],
-                                              dt.datetime.fromtimestamp(info[x][2]).strftime("%H:%M"))
-
-                    if x == len(info) - 1:
-                        output += ".\n"
-                    else:
-                        output += ", "
-    return output
-
-
-def gtfs_get_info(feed, conn, stop):
-    info = list()
-    curr = conn.cursor()
-    for entity in feed.entity:
-        if entity.HasField('trip_update'):
-            for update in entity.trip_update.stop_time_update:
-                if update.HasField('stop_id') and update.stop_id == stop:
-                    trip_id = entity.trip_update.trip.trip_id
-                    route_id = entity.trip_update.trip.route_id
-                    # TODO: move this query junk to other function. keep all the squirrels in one place
-                    headsign = ""
-                    for row in curr.execute('SELECT trip_headsign FROM trips WHERE trip_id = :trip_id',
-                                            {'trip_id': trip_id}):
-                        headsign = row[0]
-                    arrv_time = int()
-                    if update.HasField('arrival') and update.arrival.HasField('time'):
-                        arrv_time = update.arrival.time
-                    dprt_time = int()
-                    if update.HasField('departure') and update.departure.HasField('time'):
-                        dprt_time = update.departure.time
-                    stop_time = max(arrv_time, dprt_time)
-                    if stop_time > dt.datetime.now().timestamp():
-                        info.append((route_id, headsign, stop_time))
-    info.sort(key=itemgetter(2))
-    return info
-
-
 @app.route('/')
 def index() -> str:
     return render_template('index.html',
-                           signboard=signboard_from_realtime([i[0] for i in config['signboard']['stops']]))
+                           signboard='placeholder!')
 
 
 if __name__ == '__main__':
+    # uncomment to update static database:
     # gtfs_initialize_database()
-    gtfs_realtime_request('json')
+    # uncomment to request realtime json file update, for reference
+    # gtfs_realtime_request('json')
 
     sign_a = signboard_from_realtime([i[0] for i in config['signboard']['stops']])
-
-    print(sign_a.sign_time)
-    print(sign_a.stops[0].stop_id)
-    print(sign_a.stops[0].routes[0].route_id)
-    print(sign_a.stops[0].routes[0].trips[0].trip_id)
-    print(sign_a.stops[0].routes[0].trips[0].bus_time)
 
     app.run()
